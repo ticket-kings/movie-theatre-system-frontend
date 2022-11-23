62d31d547d5163da1ffbcf35d3f475452516d4c2
geerma <geerm1997@gmail.com>
2022-11-23 16:21:09 -0700

    Add Movie Page and Components

---
+++ src/Pages/Moviepage/Moviepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./moviepage.css"
+
+// Page for Individual Movies
+const Moviepage = () => {
+  return (
+    <div>Moviepage</div>
+  )
+}
+
+export default Moviepage
