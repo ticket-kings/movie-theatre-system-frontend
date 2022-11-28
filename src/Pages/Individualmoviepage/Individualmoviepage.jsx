9cb6d62245dab19bca8db4ade220f2d37531facf
geerma <geerm1997@gmail.com>
2022-11-28 14:06:49 -0700

    Showtime and seat selection from backend

--- src/Pages/Individualmoviepage/Individualmoviepage.jsx
+++ src/Pages/Individualmoviepage/Individualmoviepage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Header } from "../../Components/Header/Header";
 import seatIcon from "../../Assets/seatinverse.png";
 import { Back } from "../../Components/Back/Back";
@@ -7,62 +8,65 @@ import "./individualmoviepage.css";
 const Individualmoviepage = () => {
   const [seats, setSeats] = useState([]);
   const [movie, setMovie] = useState({});
+  const [showtime, setShowtime] = useState([]);
 
   const [chosenShowtime, setChosenShowtime] = useState();
-  const [chosenSeat, setChosenSeat] = useState();
+  const [chosenSeat, setChosenSeat] = useState({});
 
-  const chooseShowtime = (showtime) => {
-    setChosenShowtime(showtime);
-    // Fetch new seats for the specific showtime
-  };
-
-  const chooseSeat = (seat) => {
-    setChosenSeat(seat.seatNumber);
-  };
-
-  const navigateToPaymentPage = () => {
-    //navigate(`/movie/`+movie.id+'/ticket')
-    navigate('/ticket');
-  };
+  const params = useParams();
 
   const backend_endpoint = "http://localhost:8080";
 
   const fetchMovie = async () => {
-    await fetch(`${backend_endpoint}/api/v1/movie`)
+    await fetch(`${backend_endpoint}/api/v1/movie/${params.movieId}`)
       .then((res) => res.json())
-      .then((data) => setMovie(data[0])) // Change this dynamically for chosen movie!
+      .then((data) => setMovie(data)) // Change this dynamically for chosen movie!
       .catch((error) => console.log(error));
   };
 
-  const fetchSeats = async () => {
-    await fetch(`${backend_endpoint}/api/v1/showtime`)
+  const fetchShowtimes = async () => {
+    await fetch(`${backend_endpoint}/api/v1/movie/${params.movieId}/showtimes`)
       .then((res) => res.json())
-      .then((data) => setSeats(data[0].seats)) // Change this dynamically for chosen showtime!
+      .then((data) => setShowtime(data))
       .catch((error) => console.log(error));
   };
 
   useEffect(() => {
     fetchMovie();
-    fetchSeats();
+    fetchShowtimes();
   }, []);
+
+  const chooseShowtime = (index) => {
+    setChosenShowtime(showtime[index].id);
+    setSeats(showtime[index].seats);
+  };
+
+  const chooseSeat = (seat) => {
+    setChosenSeat(seat);
+  };
+
+  const navigateToPaymentPage = () => {
+    //navigate(`/movie/`+movie.id+'/ticket')
+    navigate("/ticket");
+  };
 
   return (
     <div className="moviepage_container">
       <Header />
       <div className="individualmovie_container">
-        <h1>{movie.name}</h1>
-        <img src={movie.imageUrl} width={400} height={600} />
-        <p>{movie.description}</p>
+        <h1>{movie && movie.name}</h1>
+        <img src={movie && movie.imageUrl} width={400} height={600} />
+        <p>{movie && movie.description}</p>
         <p>$10.00</p>
       </div>
 
       <div>
         <h2>Showtimes:</h2>
-        <button onClick={() => chooseShowtime(1)}>
-          Showtime 1 (not fully implemented yet)
+        <button onClick={() => chooseShowtime(0)}>
+          {showtime[0] && <p>Showtime {showtime[0].id} at {showtime[0].time}</p>}
         </button>
-        <button onClick={() => chooseShowtime(2)}>
-          Showtime 2 (not fully implemented yet)
+        <button onClick={() => chooseShowtime(1)}>
+          {showtime[1] && <p>Showtime {showtime[1].id} at {showtime[1].time}</p>}
         </button>
         <p>Your chosen showtime is: {chosenShowtime}</p>
       </div>
@@ -97,12 +101,19 @@ const Individualmoviepage = () => {
               </div>
             ))}
         </div>
-        <p>Your chosen seat is: {chosenSeat}</p>
+        {chosenSeat.seatNumber ? (
+          <p>
+            Your chosen seat is: {chosenSeat.seatNumber} (showtime ID{" "}
+            {chosenSeat.showtimeId})
+          </p>
+        ) : (
+          <p></p>
+        )}
       </div>
       <div className="addtocart_button">
         <button onClick={() => navigateToPaymentPage()}>Get Tickets</button>
       </div>
-      <Back/>
+      <Back />
     </div>
   );
 };
